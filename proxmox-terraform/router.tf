16610d75a1a197d0945125b32b2dc421c197cca7
Николай Витальевич Никоноров <bit@bitt.moe>
2026-08-27 21:07:59 +0000

    add router-1 vm

---
+++ proxmox-terraform/router.tf
@@ -0,0 +1,36 @@
+resource "proxmox_virtual_environment_vm" "router" {
+  name      = "router-1"
+  node_name = var.proxmox_node
+
+  clone {
+    vm_id        = var.template_vm_id
+    full         = true
+    datastore_id = var.vm_datastore_id
+  }
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  initialization {
+    datastore_id = var.vm_datastore_id
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
